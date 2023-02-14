dc4ce6c136c992707231f8a3dbe75da0d739a8ac
Connor <cgbroadfoot@gmail.com>
2023-02-14 02:32:23 +0000

    Update query to join points table so that the map and all of its points are returned

--- db/queries/map-by-id.js
+++ db/queries/map-by-id.js
@@ -1,8 +1,12 @@
 const db = require('../connection');
 
 const getMapByID = (id) => {
-  return db.query(`SELECT * FROM maps WHERE id = ${id} ;`)
+  return db.query(
+    `SELECT maps.*, points.* FROM maps
+    JOIN points ON maps.id = map_id
+    WHERE maps.id = ${id} ;`)
     .then(data => {
+      console.log(data)
       return data.rows[0];
     });
 };
