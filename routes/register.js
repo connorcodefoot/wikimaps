c82cc39ebac7e6aa27eef15397d4df4249a06b39
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-14 18:24:31 -0800

    added reroute if already logged in and registration form

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,7 @@
-const express = require ('express');
-const router = express.Router();
+const express = require('express');
+const router  = express.Router();
 const users = require('../db/queries/users');
+
 
 const findEmail = (email, database) => {
   for (const users in database) {
@@ -14,28 +15,32 @@ const findEmail = (email, database) => {
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
-
+//if logged in reroutes to login page
 router.get('/', (req, res) => {
-  // if (req.session.userID){
-  //   res.redirect('/login');
-  //   return;
-  // }
+  if (req.session.email){
+    res.redirect('/login');
+    return;
+  }
   res.render('register');
 });
 
-  router.post('/', (req, res) => {
-    if (req.body.email && req.body.password) {
-      if (!findEmail(req.body.email, users)) {
+//registration functionality
+
+router.post('/', (req, res) => {
+  let email = req.body.email
+  let password = req.body.password
+
+    if (email && password) {
+      if (!findEmail(email, users)) {
         const userID = generateRandomString();
         users[userID] = {
           userID,
           email: req.body.email,
           password: req.body.password
         }
-        req.session.userID  = userID;
-        res.redirect('/login');
-      } else {
-        res.statusCode = 400;
+        req.session.userID = userID
+        res.redirect('/login')
+      } else { res.statusCode = 400;
         res.send('<h2>400  Bad Request<br>Email already registered.</h2>')
       }
     } else {
@@ -43,3 +48,19 @@ router.get('/', (req, res) => {
       res.send('<h2>400  Bad Request<br>Please fill out the email and password fields.</h2>')
     }
   });
+
+
+
+
+
+  //     } else {
+  //       res.statusCode = 400;
+  //       res.send('<h2>400  Bad Request<br>Email already registered.</h2>')
+  //     }
+  //   } else {
+  //     res.statusCode = 400;
+  //     res.send('<h2>400  Bad Request<br>Please fill out the email and password fields.</h2>')
+  //   }
+  // });
+
+module.exports = router;
