1e048d4d7d440f9eca9090efb253528178c716c2
Connor <cgbroadfoot@gmail.com>
2023-02-15 22:06:42 +0000

    Create a new query that will update a point when it is changed on points/edit/id

---
+++ db/queries/update-point.js
@@ -0,0 +1,8 @@
+const db = require('../connection');
+
+const updatePointByID = (req, id) => {
+  return db.query(
+    `UPDATE points SET title = ${req.title}, description = ${req.description}, latitude = ${req.latitude}, longitude = ${req.longitude}, image = ${req.image} WHERE id = ${id};`)
+  };
+
+module.exports = { updatePointByID };
