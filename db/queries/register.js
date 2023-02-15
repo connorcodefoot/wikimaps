33b3843253b2c7ef86bd3dd96063539e58c83f87
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-15 15:36:02 -0800

    added registration confirmation with reroutes to login

--- db/queries/register.js
+++ db/queries/register.js
@@ -1,9 +1,16 @@
 const db = require('../connection')
 
-const addUser = (req) => {
-  return db.query(
-    `INSERT INTO points (id, email, password) VALUES (${req.id}, ${req.email}, ${req.password})`
-  )
-};
+// const addUser = (email, password) => {
+//   return db.query(
+//     `INSERT INTO users (email, password) VALUES (${email}, ${password})`
+//   )
+// };
+const addUser = function(email, password) {
+  const queryString = `
+  INSERT INTO users (email, password)
+  VALUES ($1, $2);`
+  const values = [email, password]
+  return db.query(queryString, values)
+}
 
-module.exports= { addUser }
+module.exports= { addUser };
--- routes/register.js
+++ routes/register.js
@@ -1,65 +1,28 @@
 const express = require('express');
 const router  = express.Router();
 const users = require('../db/queries/users');
-const addUser = require('../db/queries/register')
+const register = require('../db/queries/register')
 
 
-const findEmail = (email, database) => {
-  for (const users in database) {
-    if (database[users].email === email) {
-      return database[users];
-    }
-  }
-  return undefined;
-};
-
-function generateRandomString() {
-  return Math.random().toString(36).substring(2, 8);
-}
-//if logged in reroutes to home page
 router.get('/', (req, res) => {
   res.render('register');
 });
 
 //registration functionality
 
-router.post('/register', (req, res) => {
+router.post('/', (req, res) => {
   let email = req.body.email
   let password = req.body.password
   if (email && password) {
-    if (!findEmail(email, users)) {
-  register.addUser(req.body)
+  register.addUser(email, password)
   .then(() => {
-    res.redirect(`/login`)
+    res.redirect(`/registration-confirmation`)
   })
   .catch(err => {
     res.status(400).json({ error: err.message });
       })
-    }
   }
 })
 
-
-// router.post('/', (req, res) => {
-//   let email = req.body.email
-//   let password = req.body.password
-//     if (email && password) {
-//       if (!findEmail(email, users)) {
-//         const userID = generateRandomString();
-//         addUser[userID] = {
-//           userID,
-//           email: req.body.email,
-//           password: req.body.password
-//         }
-//         req.session.userID = userID
-//         res.redirect('/')
-//       } else { res.statusCode = 400;
-//         res.send('<h2>400  Bad Request<br>Email already registered.</h2>')
-//       }
-//     } else {
-//       res.statusCode = 400;
-//       res.send('<h2>400  Bad Request<br>Please fill out the email and password fields.</h2>')
-//     }
-//   });
 
 module.exports = router;
