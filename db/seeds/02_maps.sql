b87414e323047a1884d7f8e634937a56db18fa4d
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-16 21:25:38 -0800

    change seed data

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,3 @@
-INSERT INTO maps (name, lat, lng, zoom) VALUES ('ahsfa', -33.8462, 151.2489, 4);
-INSERT INTO maps (name, lat, lng, zoom) VALUES ('Bod', -33.8568, 151.2153, 4);
+INSERT INTO maps (name, lat, lng, zoom) VALUES ('Computer Stores', -33.8462, 151.2489, 4);
+INSERT INTO maps (name, lat, lng, zoom) VALUES ('Gyms', -33.8568, 151.2153, 4);
 INSERT INTO maps (name, lat, lng, zoom) VALUES ('Vancouver Coffee Shops', 49.3043, -123.1443, 11);
