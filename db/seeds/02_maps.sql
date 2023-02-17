f48db72fb03f3387279691eb79f98394b35ae673
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-16 22:19:09 -0800

    mergeing

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,4 +1,4 @@
 INSERT INTO maps (name, lat, lng, zoom) VALUES ('Computer Stores', 49.3043, -123.1443, 11);
 INSERT INTO maps (name, lat, lng, zoom) VALUES ('Gyms', 49.3043, -123.1443, 11);
 INSERT INTO maps (name, lat, lng, zoom) VALUES ('Vancouver Coffee Shops', 49.3043, -123.1443, 11);
-INSERT INTO maps (name, lat, lng, zoom) VALUES ('Github Sucks', 37.782190, -122.391280, 11);
+INSERT INTO maps (name, lat, lng, zoom) VALUES ('Github Error Meetups', 37.782190, -122.391280, 11);
